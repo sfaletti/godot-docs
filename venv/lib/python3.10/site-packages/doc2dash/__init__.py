bdf84d5a70b5a8a37b05ceccc5ee2b045293981d
Steve Faletti <stevefaletti@gmail.com>
2022-08-07 09:33:16 -0400

    Add doc2dash publishing package

---
+++ venv/lib/python3.10/site-packages/doc2dash/__init__.py
@@ -0,0 +1,17 @@
+"""
+Convert docs to the docset format.
+"""
+
+
+__version__ = "2.4.1"
+
+
+__title__ = "doc2dash"
+__description__ = "Convert docs to the docset format."
+__url__ = "https://doc2dash.readthedocs.io/"
+
+__author__ = "Hynek Schlawack"
+__email__ = "hs@ox.cx"
+
+__license__ = "MIT"
+__copyright__ = f"Copyright (c) 2012 {__author__}"
